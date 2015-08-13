330c9990c3be4a2148fe085a37da3e99da293377
Michael Berkovich <michael@translationexchange.com>
2015-08-13 13:49:30 -0700

    fixed tests

--- test/express_test.js
+++ test/express_test.js
@@ -1,6 +1,6 @@
 var assert = require("assert");
 
-describe('Tr8n Express', function(){
+describe('Tml Express', function(){
   describe('creation', function(){
     it('should correctly run a test', function() {
       assert.ok(true);
